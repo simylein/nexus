319a97f0ff20b3c179fef54d8ba87d316ed4dbee
simylein <67859356+simylein@users.noreply.github.com>
2025-06-16 18:39:42 +0200

    feat(main): make main thread serve health check

--- src/main.c
+++ src/main.c
@@ -7,12 +7,36 @@
 #include "seed.h"
 #include "spi.h"
 #include "thread.h"
+#include <arpa/inet.h>
+#include <signal.h>
 #include <sqlite3.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+int server_sock;
+struct sockaddr_in server_addr;
+
+worker_t *workers;
+
+void stop(int sig) {
+	signal(sig, SIG_DFL);
+	trace("received signal %d\n", sig);
+
+	if (close(server_sock) == -1) {
+		error("failed to close socket because %s\n", errno_str());
+	}
+
+	free(workers);
+
+	info("graceful shutdown complete\n");
+	exit(0);
+}
+
 int main(int argc, char *argv[]) {
+	signal(SIGINT, &stop);
+	signal(SIGTERM, &stop);
+
 	if (argc >= 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)) {
 		info("available command line flags\n");
 		info("--name              -n   name of application            (%s)\n", name);
@@ -114,7 +138,7 @@ int main(int argc, char *argv[]) {
 		error("failed to close %s because %s\n", database_file, sqlite3_errmsg(database));
 	}
 
-	worker_t *workers = malloc(radios_len * sizeof(worker_t));
+	workers = malloc(radios_len * sizeof(worker_t));
 	if (workers == NULL) {
 		fatal("failed to allocate %zu bytes for workers because %s\n", radios_len * sizeof(worker_t), errno_str());
 		exit(1);
@@ -132,8 +156,89 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	// FIXME make main thread do something useful
+	info("spawned %hhu worker threads\n", radios_len);
+
+	if ((server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+		fatal("failed to create socket because %s\n", errno_str());
+		exit(1);
+	}
+
+	if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (int[]){1}, sizeof(int)) == -1) {
+		fatal("failed to set socket reuse address because %s\n", errno_str());
+		exit(1);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(address);
+	server_addr.sin_port = htons(port);
+
+	if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+		fatal("failed to bind to socket because %s\n", errno_str());
+		exit(1);
+	}
+
+	if (listen(server_sock, backlog) == -1) {
+		fatal("failed to listen on socket because %s\n", errno_str());
+		exit(1);
+	}
+
+	info("listening on %s:%d\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+
 	while (true) {
-		sleep(60);
+		struct sockaddr_in client_addr;
+		int client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &(socklen_t){sizeof(client_addr)});
+
+		if (client_sock == -1) {
+			error("failed to accept client because %s\n", errno_str());
+			continue;
+		}
+
+		char request_buffer[2048];
+		char response_buffer[2048];
+
+		ssize_t bytes_received = recv(client_sock, request_buffer, sizeof(request_buffer), 0);
+
+		if (bytes_received == -1) {
+			error("failed to receive data from client because %s\n", errno_str());
+			goto cleanup;
+		}
+		if (bytes_received == 0) {
+			warn("client did not send any data\n");
+			goto cleanup;
+		}
+
+		trace("received %zd bytes from %s:%d\n", bytes_received, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+		const char request[] = "HEAD / HTTP/1.1\r\n";
+		size_t response_length = 0;
+
+		if ((size_t)bytes_received >= sizeof(request) - 1 && memcmp(request_buffer, request, sizeof(request) - 1) == 0) {
+			const char response[] = "HTTP/1.1 200 OK\r\n\r\n";
+			memcpy(response_buffer, response, sizeof(response) - 1);
+			response_length += sizeof(response) - 1;
+			info("acknowledged heartbeat request\n");
+		} else {
+			const char response[] = "HTTP/1.1 404 Not Found\r\n\r\n";
+			memcpy(response_buffer, response, sizeof(response) - 1);
+			response_length += sizeof(response) - 1;
+		}
+
+		ssize_t bytes_sent = send(client_sock, response_buffer, response_length, MSG_NOSIGNAL);
+
+		if (bytes_sent == -1) {
+			error("failed to send data to client because %s\n", errno_str());
+			goto cleanup;
+		}
+		if (bytes_sent == 0) {
+			warn("server did not send any data\n");
+			goto cleanup;
+		}
+
+		trace("sent %zd bytes to %s:%d\n", bytes_sent, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+	cleanup:
+		if (close(client_sock) == -1) {
+			error("failed to close client socket because %s\n", errno_str());
+		}
 	}
 }
