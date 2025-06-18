185e8720d938015839b0a6a7f6fa5285fc9e9293
simylein <67859356+simylein@users.noreply.github.com>
2025-06-18 19:09:57 +0100

    fix(radio): correctly use stack pointers

--- src/radio.c
+++ src/radio.c
@@ -1,6 +1,7 @@
 #include "radio.h"
 #include "logger.h"
 #include <sqlite3.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -61,16 +62,16 @@ uint16_t radio_select(sqlite3 *database, radio_t (*radios)[16], uint8_t *radios_
 			const uint8_t tx_power = (uint8_t)sqlite3_column_int(stmt, 6);
 			const uint8_t sync_word = (uint8_t)sqlite3_column_int(stmt, 7);
 			const bool checksum = (bool)sqlite3_column_int(stmt, 8);
-			memcpy(radios[*radios_len]->id, id, id_len);
-			memcpy(radios[*radios_len]->device, device, device_len);
-			radios[*radios_len]->device_len = (uint8_t)device_len;
-			radios[*radios_len]->frequency = frequency;
-			radios[*radios_len]->bandwidth = bandwidth;
-			radios[*radios_len]->coding_rate = coding_rate;
-			radios[*radios_len]->spreading_factor = spreading_factor;
-			radios[*radios_len]->tx_power = tx_power;
-			radios[*radios_len]->sync_word = sync_word;
-			radios[*radios_len]->checksum = checksum;
+			memcpy((*radios)[*radios_len].id, id, id_len);
+			memcpy((*radios)[*radios_len].device, device, device_len);
+			(*radios)[*radios_len].device_len = (uint8_t)device_len;
+			(*radios)[*radios_len].frequency = frequency;
+			(*radios)[*radios_len].bandwidth = bandwidth;
+			(*radios)[*radios_len].coding_rate = coding_rate;
+			(*radios)[*radios_len].spreading_factor = spreading_factor;
+			(*radios)[*radios_len].tx_power = tx_power;
+			(*radios)[*radios_len].sync_word = sync_word;
+			(*radios)[*radios_len].checksum = checksum;
 			*radios_len += 1;
 		} else if (result == SQLITE_DONE) {
 			status = 0;
